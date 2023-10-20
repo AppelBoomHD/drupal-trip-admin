f6850690ad6d79b82897a4189209cf0432fa5719
AppelBoomHD <julianriemersma@gmail.com>
2023-10-20 14:11:44 +0200

    refactor(stop): remove unused dependency

--- src/Entity/Stop.php
+++ src/Entity/Stop.php
@@ -6,11 +6,9 @@ namespace Drupal\trip_admin\Entity;
 
 use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Entity\EntityChangedTrait;
-use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\trip_admin\StopInterface;
-use Drupal\user\EntityOwnerTrait;
 
 /**
  * Defines the stop entity class.
